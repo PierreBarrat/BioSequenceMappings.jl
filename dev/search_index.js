244a0bbf68664c3b090e5d3f637b79d36d5280f8
Documenter.jl <documenter@juliadocs.github.io>
2024-04-08 16:11:03 +0000

    build based on 4c35710

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BioSequenceMappings","category":"page"},{"location":"#BioSequenceMappings","page":"Home","title":"BioSequenceMappings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BioSequenceMappings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BioSequenceMappings]","category":"page"}]
+}
