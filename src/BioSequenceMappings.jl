4acee857792b4ef835f2ccedbe7b1455fe07eb1e
PierreBarrat <p.barrat@live.fr>
2024-04-08 18:09:45 +0200

    work

--- src/BioSequenceMappings.jl
+++ src/BioSequenceMappings.jl
@@ -1,5 +1,28 @@
 module BioSequenceMappings
 
-# Write your package code here.
+using FASTX
+using OneHotArrays
+using Random
+
+import Base: length, size
+import Base: in, ==, hash, convert, copy
+import Base: getindex, firstindex, lastindex, eachindex, view, keys
+import Base: iterate, eltype
+import Base: unique
+import Base: write
+
+include("alphabet.jl")
+export Alphabet
+export default_alphabet
+
+include("alignment.jl")
+export Alignment
+export sequence_length, sequence_number, subsample
+
+include("weights.jl")
+export compute_weights
+
+include("IO.jl")
+export read_fasta
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using BioSequenceMappings
+using DCAUtils # to compare weights
+using Random
 using Test
 
+here = dirname(@__FILE__)
+
 @testset "BioSequenceMappings.jl" begin
-    # Write your tests here.
+    @testset "Alphabet" begin
+        println("# Alphabet")
+        include(joinpath(here, "./alphabets/test.jl"))
+    end
+    @testset "Alignment" begin
+        println("# Alignment")
+        include(joinpath(here, "./alignments/test.jl"))
+    end
+    @testset "IO" begin
+        println("# IO")
+        include(joinpath(here, "./io/test.jl"))
+    end
 end
