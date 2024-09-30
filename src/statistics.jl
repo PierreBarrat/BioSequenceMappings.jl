ed10ae073a053cd484070a4f0c81e16643b90e3f
PierreBarrat <p.barrat@live.fr>
2024-09-30 17:01:46 +0200

    fix as_mat for f2 and C - weights default to alignment weights - add docstrings

--- src/statistics.jl
+++ src/statistics.jl
@@ -23,14 +23,14 @@ function f1(X::AbstractAlignment, weights::Tuple{Any,Any})
 end
 
 """
-    site_specific_frequencies(X::AbstractAlignment, weights=nothing; as_vec=true)
+    site_specific_frequencies(X::AbstractAlignment[, weights=X.weights]; as_vec=false)
 
 Return the site specific frequencies of `X`.
-If `as_vec` (default), the result is a vector of length `Lxq`.
-Otherwise, it is a matrix of `q` rows and `L` columns
+If `as_vec`, the result is a vector of length `Lxq`.
+Otherwise, it is a matrix of `q` rows and `L` columns (default).
 """
-function site_specific_frequencies(X, args...; as_vec=false)
-    f = f1(X, args...)
+function site_specific_frequencies(X, w=X.weights; as_vec=false)
+    f = f1(X, w)
     if as_vec
         q, L = size(f)
         return reshape(f, q*L)
@@ -38,7 +38,7 @@ function site_specific_frequencies(X, args...; as_vec=false)
     return f
 end
 
-function consensus(X::AbstractAlignment, w=nothing)
+function consensus(X::AbstractAlignment, w=X.weights)
     f = site_specific_frequencies(X, w; as_vec=false)
     cons = map(argmax, eachcol(f))
     return Alignment(cons, Alphabet(X); names = ["consensus"])
@@ -65,19 +65,36 @@ f2(X) = f2(X, ones(Float64, size(X,2))/size(X,2))
 f2(X, ::Nothing) = f2(X)
 f2(X, weights::Tuple{Any,Any}) = f2(X, weights[1])
 
-function pairwise_frequencies(X::AbstractAlignment, args...; as_mat=false)
-    f = f2(X, args...)
+"""
+    pairwise_frequencies(X::AbstractAlignment, w=X.weights; as_mat=false)
+
+Return a `q x q x L x L` tensor.
+The `(a, b, i, j)` element is the fraction of sequences for which we see `a` at position
+`i` and `b` at position `j`.
+
+If `as_mat=true`, will return a `qL x qL` matrix, with `q x q` blocks representing
+correlations between two specific columns.
+"""
+function pairwise_frequencies(X::AbstractAlignment, w=X.weights; as_mat=false)
+    f = f2(X, w)
     if as_mat
         q, L = size(f, 1), size(f, 3)
-        permutedims!(f, [1, 3, 2, 4])
+        f = permutedims(f, [1, 3, 2, 4])
         return reshape(f, q*L, q*L)
     end
     return f
 end
 
-function pairwise_correlations(X::AbstractAlignment, args...; as_mat=false)
-    f1 = site_specific_frequencies(X)
-    f2 = pairwise_frequencies(X)
+"""
+    pairwise_correlations(X, w=X.weights; as_mat=false)
+
+Compute connected correlations: the difference between the pairwise frequencies and the
+product of the single site frequencies.
+See `?pairwise_frequencies` for the shape of the output.
+"""
+function pairwise_correlations(X::AbstractAlignment, w=X.weights; as_mat=false)
+    f1 = site_specific_frequencies(X, w)
+    f2 = pairwise_frequencies(X, w)
 
     q, L = size(f1)
     C = zeros(Float64, q, q, L, L)
@@ -87,7 +104,7 @@ function pairwise_correlations(X::AbstractAlignment, args...; as_mat=false)
         C[b, a, j, i] = C[a, b, i, j]
     end
     if as_mat
-        permutedims!(C, [1, 3, 2, 4])
+        C = permutedims(C, [1, 3, 2, 4])
         return reshape(C, q*L, q*L)
     end
     return C
